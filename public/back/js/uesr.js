73df3ec7cc2e9fc24a83ff598ff224923cbb85c3
xiaminghong <1562911656@qq.com>
2018-10-31 22:20:22 +0800

    管理系统/主页完成

---
+++ public/back/js/uesr.js
@@ -0,0 +1,18 @@
+
+$(function(){
+  $.ajax({
+    type:'get',
+    url:'/user/queryUser',
+    data:{
+      page:1,
+      pageSize:5
+    },
+    dataType:'json',
+    success:function(info){
+      console.log(info);
+      var htmlStr=template('tmp',info);
+      $('tbody').html(htmlStr);
+      
+    }
+  })
+})
