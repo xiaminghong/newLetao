af9cc7ed1c492e13d0453f47a0971b07003035d4
xiaminghong <1562911656@qq.com>
2018-10-31 20:26:12 +0800

    用户退出及二级导航完成

--- public/back/js/common.js
+++ public/back/js/common.js
@@ -1,4 +1,7 @@
  
+
+
+
 $(document).ajaxStart(function(){
   NProgress.start(); 
 })
@@ -6,3 +9,38 @@ $(document).ajaxStop(function(){
   NProgress.done();
 })
   
+
+$(function(){
+  // 二级分类
+  $('.nav .category').on('click',function(){
+   $(this).next().stop().slideToggle();
+  })
+  // 菜单
+  $('.icon_menu').on('click',function(){
+    $('.lt_aside ').toggleClass('hidemenu');
+    $('.lt_main').toggleClass('hidemenu');
+    $('.lt_topber').toggleClass('hidemenu');
+    
+  })
+  // 退出
+  $('.icon_logout').on('click',function(){
+    $('#logoutModal').modal("show");
+    
+  })
+  $('#logoutBtn').click(function(){
+      $.ajax({
+        type:"get",
+        url: "/employee/employeeLogout",
+        dataType: "json",
+        success:function(info){
+          console.log(info);
+          if(info.success){
+            location.href="login.html";
+          }
+        }
+      })    
+  })
+
+
+
+})
